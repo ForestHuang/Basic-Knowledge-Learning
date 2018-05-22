bcfb76c0ddb094bd15cddc57af84d6c6b7a82184
森霖 黄 <huangmumumu@outlook.com>
2018-05-22 16:45:01 +0800

    汉字转换成拼音

--- SURE.BasicLearningApplication/02 - Reflection/AssemblyReflection.cs
+++ SURE.BasicLearningApplication/02 - Reflection/AssemblyReflection.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using SURE.Common;
+using System;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SURE.BasicLearningApplication._02___Reflection
 {
     class AssemblyReflection
     {
         /**
-            C#写的代码最终生成dll文件，也就是程序集。
-
-            dll = (IL + metedata:元数据)
-
-            .Net中获取运行时类型信息的方式，.Net的应用程序由几个部分：‘程序集(Assembly)’、‘模块(Module)’、‘类型(class)’组成，而反射提供一种编程的方式，
-            让程序员可以在程序运行期获得这几个组成部分的相关信息
-            
-            反射：通过程序集（dll、exe）运行时动态的创建类型实例，即使这个对象的类型在编译时还不知道，以及获取相关信息每一个类型（包括类、委托、方法、构造器、属性等等）;
-            
-            反射破坏单例模式，使单例失效，一样的多次创建对象；
+         *  C#写的代码最终生成dll文件，也就是程序集。
+         *
+         *  dll = (IL + metedata:元数据)
+         *
+         *  .Net中获取运行时类型信息的方式，.Net的应用程序由几个部分：‘程序集(Assembly)’、‘模块(Module)’、‘类型(class)’组成，而反射提供一种编程的方式，
+         *  让程序员可以在程序运行期获得这几个组成部分的相关信息
+         *  
+         *  反射：通过程序集（dll、exe）运行时动态的创建类型实例，即使这个对象的类型在编译时还不知道，以及获取相关信息每一个类型（包括类、委托、方法、构造器、属性等等）;
+         *  还可以获取特性，通过AOP面向切面编程
+         *  动态: 接触依赖降低耦合可扩展，不需要事先引用
+         *  好处：灵活、可扩展性、动态
+         *  反射的局限性：
+         *  反射破坏单例模式，使单例失效，一样的多次创建对象；
         **/
 
         //Reflection 反射
@@ -37,17 +37,52 @@ namespace SURE.BasicLearningApplication._02___Reflection
             var types = assembly.GetTypes();
             //var type = assembly.GetType("SURE.DataEntity.Reflection.People");
 
-            Console.WriteLine("------------------------程序集全部类型-------------------------");
+            //--------------GetModules 获取Module 找出当前dll所有的模块
+            Console.WriteLine(">>------------------------程序集全部模块------------------------->>");
+            foreach (Module module in assembly.GetModules())
+            {
+                Console.WriteLine($" ModuleName: {module.Name}");
+            }
+
+            Console.WriteLine("\n>>------------------------程序集全部类型------------------------->>");
             foreach (var type in types)
             {
-                Console.WriteLine($"类名: {type.Name} ,完整路径: {type.FullName} ");
+                Console.WriteLine($" ClassName: {type.Name} ,ClassFullName: {type.FullName}");
+            }
 
-                //全部方法
-                var typeMethhods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-                foreach (var typeMethhod in typeMethhods)
+            Console.WriteLine("\n>>------------------------创建当前类型------------------------->>");
+            //创建对象实例，默认调用无参构造也可调用有参构造:CreateInstance(Type type, params object[] args);
+            object obj = Activator.CreateInstance(types[0]);
+
+            //当前类型的全部方法
+            var methods = types[0].GetMethods();
+            Console.WriteLine("\n>>------------------------当前类型的所有方法------------------------->>");
+            foreach (var typeMethod in methods)
+            {
+                Console.WriteLine($" MethodName: {typeMethod.Name} ,ReturnType: {typeMethod.ReturnType}");
+            }
+
+            //调用方法
+            Console.WriteLine("\n>>------------------------调用HelloMethod方法------------------------->>");
+            var method = types[0].GetMethod("HelloMethod");
+            var result = method.Invoke(obj, null);
+            Console.WriteLine(result);
+
+            //设置属性
+            Console.WriteLine("\n>>------------------------设定属性值------------------------->>");
+            foreach (PropertyInfo propertyInfo in types[0].GetProperties())
+            {
+                Console.WriteLine($"属性名: {propertyInfo.Name}");
+                if (propertyInfo.Name == "PeopleName")
                 {
-                    Console.WriteLine($"方法名: {typeMethhod.Name}");
+                    propertyInfo.SetValue(obj, "senlin.huang");
                 }
+            }
+
+            var methodsTwo = types[0].GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (var mothod in methodsTwo)
+            {
+                Console.WriteLine(mothod.Name);
             }
         }
 
---
+++ SURE.Common/ChineseHelper.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.International.Converters.PinYinConverter;
+using System;
+using System.Text;
+
+namespace SURE.Common
+{
+    public class ChineseHelper
+    {
+        /// <summary>
+        /// 根据汉字转换成全拼
+        /// </summary>
+        /// <param name="chineseCharacters">入参汉字</param>
+        /// <param name="isLower">是否小写,默认大写</param>
+        /// <returns>string</returns>
+        public static string ConvertToAllSpelling(string chineseCharacters, bool isLower = false)
+        {
+            try
+            {
+                if (chineseCharacters.Length != 0)
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int i = 0; i < chineseCharacters.Length; i++)
+                    {
+                        var chr = chineseCharacters[i];
+                        stringBuilder.Append(GetSpell(chr));
+                    }
+
+                    if (isLower)
+                        return stringBuilder.ToString().ToLower();
+                    else
+                        return stringBuilder.ToString().ToUpper();
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"转换失败,错误: {ex.Message}";
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 汉字转首字母
+        /// </summary>
+        /// <param name="chineseCharacters">入参汉字</param>
+        /// <param name="isLower">是否小写,默认大写</param>
+        /// <returns>string</returns>
+        public static string GetFirstSpell(string chineseCharacters, bool isLower = false)
+        {
+            try
+            {
+                if (chineseCharacters.Length != 0)
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int i = 0; i < chineseCharacters.Length; i++)
+                    {
+                        var chineses = chineseCharacters[i];
+                        stringBuilder.Append(GetSpell(chineses)[0]);
+                    }
+                    if (isLower)
+                        return stringBuilder.ToString().ToLower();
+                    else
+                        return stringBuilder.ToString().ToUpper();
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"转换失败,错误: {ex.Message}";
+            }
+            return string.Empty;
+        }
+
+        #region Private
+
+        /// <summary>
+        /// 单个字母转换成全拼
+        /// </summary>
+        /// <param name="chineseCharacters">单个字</param>
+        /// <returns>string</returns>
+        private static string GetSpell(char chineseCharacters)
+        {
+            var resultPinyins = NPinyin.Pinyin.GetPinyin(chineseCharacters);
+            bool isChineses = ChineseChar.IsValidChar(resultPinyins[0]);
+            if (isChineses)
+            {
+                ChineseChar chineseChar = new ChineseChar(resultPinyins[0]);
+                foreach (string value in chineseChar.Pinyins)
+                {
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        return value.Remove(value.Length - 1, 1);
+                    }
+                }
+            }
+            return resultPinyins;
+        }
+
+        #endregion
+
+    }
+}
