405243d13449844e67e59dfdac38203175003eea
senlin.huang <senlin.huang@DESKTOP-DA4O6AC>
2018-05-24 06:53:25 +0800

    事件、委托

---
+++ SURE.BasicLearningApplication/03 - delegate、event/delegateAndevent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SURE.BasicLearningApplication._03___delegate_event
+{
+    /// <summary>
+    /// 事件、委托
+    /// </summary>
+    class delegateAndevent
+    {
+    }
+}
