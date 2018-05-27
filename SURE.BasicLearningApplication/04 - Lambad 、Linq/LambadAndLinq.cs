38f431fbd6d1039fa4c51cc81025a87b108015ed
senlin.huang <senlin.huang@DESKTOP-DA4O6AC>
2018-05-27 21:24:18 +0800

    Lambad

--- SURE.BasicLearningApplication/04 - Lambad 、Linq/LambadAndLinq.cs
+++ SURE.BasicLearningApplication/04 - Lambad 、Linq/LambadAndLinq.cs
@@ -8,5 +8,25 @@ namespace SURE.BasicLearningApplication._04___Lambad__Linq
 {
     class LambadAndLinq
     {
+        /*
+            Lambad 表达式
+            本质：匿名方法，Lambad就是个方法匿名方法；
+            系统自带委托: Action,Func
+        */
+
+        //创建有参有返回委托
+        private delegate string LambadDelegate(string name);
+
+        static void Main(string[] args)
+        {
+            LambadDelegate lambad = (x) =>
+            {
+                Console.WriteLine(x);
+                return x;
+            };
+
+            var result = lambad.Invoke("senlin.huang");
+            Console.WriteLine(result);
+        }
     }
 }
