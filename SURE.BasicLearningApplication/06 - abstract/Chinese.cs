b61fd524adec9d264531e9d20b9a5016db6702ea
森霖 黄 <huangmumumu@outlook.com>
2018-06-04 17:30:00 +0800

    sure abstract

---
+++ SURE.BasicLearningApplication/06 - abstract/Chinese.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SURE.BasicLearningApplication._06___abstract
+{
+    public class Chinese : BasePeople
+    {
+        public override int Age()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Sex()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
